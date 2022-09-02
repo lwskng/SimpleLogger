4a03fc0573c214edab6670c6460a97e620ac08a4
lwskng <lwskng@gmail.com>
2022-09-02 23:48:56 +0000

    Initial commit

--- usage_example.cpp
+++ usage_example.cpp
@@ -7,6 +7,7 @@
 
 using namespace std::string_literals;
 
+//dummy func 1
 auto some_func = [](std::ostream &os, size_t thread_id, int a) {
     auto res = a * a;
     os << "thread id is: " << thread_id;
@@ -17,6 +18,7 @@ auto some_func = [](std::ostream &os, size_t thread_id, int a) {
     return res;
 };
 
+//dummy func 2
 auto another_func = [](std::ostream &os, size_t thread_id, int a, double b) {
     auto res = a * b;
     os << "thread id is: " << thread_id;
@@ -30,19 +32,23 @@ auto another_func = [](std::ostream &os, size_t thread_id, int a, double b) {
 
 int main () {
 
+    //make instance
     dev_tools::Logger logger("v 0.1, side branch"s, std::cout, true);
     logger.LogMessage("Logger created"s);
 
+    //make threads
     const int thread_size = 100;
     logger.LogMessage("Making a vector of threads with size of"s, thread_size);
     std::vector<std::jthread> v(thread_size);
 
+    //lambda to generate a prn
     const auto make_argument = []() {
         unsigned int delay = rand()%100+1;
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
         return delay;
     };
 
+    //log some arbitrary messages
     for (int t_id = 0; t_id != thread_size / 2; ++t_id) {
         v[t_id] = std::jthread([t_id, &logger, &make_argument](){
             auto delay = make_argument();
@@ -50,6 +56,7 @@ int main () {
         });
     }
 
+    //log dummy func 1
     for (int t_id = thread_size / 2; t_id != thread_size * 3 / 4; ++t_id) {
         v[t_id] = std::jthread([t_id, &logger, &make_argument](){
             auto arg = make_argument();
@@ -57,6 +64,7 @@ int main () {
         });
     }
 
+    //log dummy func 2
     for (int t_id = thread_size * 3 / 4; t_id != thread_size; ++t_id) {
         v[t_id] = std::jthread([t_id, &logger, &make_argument](){
             auto arg = make_argument();
